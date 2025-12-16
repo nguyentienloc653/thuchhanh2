e78dd8dd1a8fbbcdf140ef8bd1cf811a73c0b35b
nguyentienloc653 <locnguyentien653@gmail.com>
2025-12-16 10:51:07 +0700

    .

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+// import Dashboard from "./dashboard/Dashboard";
+import HomePage from "./homePage/HomePage";
+
+function App() {
+  return (
+    <div>
+      {/* <Dashboard /> */}
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
